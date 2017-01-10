17ee5176e94b9b06c6a5a67d9c41c42d8aab431d
devansh agarwal <devansh.agarwal@sap.com>
2017-01-10 20:16:08 +0530

    Adding seeder file for users table.
    Run commands:
    * php artisan migrate:status
    * php artisan migrate
    * php artisan db:seed

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->delete();
+    
+        // users::create(array(
+        //     'name' => 'Devansh',
+        //     'email' => 'devwrap@gmail.com',
+        //     'password' => 'pass'
+        // ));
+    
+        /*DB::table('users')->insert([
+            'name' => str_random(10),
+            'email' => str_random(10).'@gmail.com',
+            'password' => bcrypt('secret'),
+        ]);*/
+
+		DB::table('users')->insert([
+            'name' => 'test1',
+            'email' => 'test1@gmail.com',
+            'password' => 'test1',
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'test2',
+            'email' => 'test2@gmail.com',
+            'password' => 'test2',
+        ]);
+    }
+}
